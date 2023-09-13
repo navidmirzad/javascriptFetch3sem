df09acf0a58c723a09cca137cfd8e3586ab09fea
navidmirzad <navi0221@stud.kea.dk>
2023-09-13 09:45:56 +0200

    fetchurl json to text

--- js/fetchurl.js
+++ js/fetchurl.js
@@ -1,28 +1,36 @@
 console.log("Im in fetchURL")
 
+/*function fetchAnyUrlText(url) {
+    console.log("Inside fetch text url = " + url)
+    return fetch(url).then(response  => response.text());
+}*/
+
+function fetchAnyUrl(url) {
+    console.log("Inside fetch url = " + url)
+    return fetch(url).then(response => response.json());
+}
+
+function getKeysAndValuesFromObj(obj) {
+    const keys = Object.keys(obj)
+    return keys.map(key => `${key} : ${obj[key]}`)
+}
+
+
+async function actionFetchUrl(btn) {
+    const url = inpUrl.value
+    console.log(url)
+    let jsonOutput = await fetchAnyUrl(url)
+    if (Array.isArray(jsonOutput)) {
+        jsonOutput = jsonOutput[0]
+    }
+    jsonOutput = getKeysAndValuesFromObj(jsonOutput)
+    textArea.textContent = jsonOutput
+    console.log(jsonOutput)
+}
 
 const inpUrl = document.getElementById("inpUrl")
 const textArea = document.getElementById("txt")
 const pbFetchUrl = document.getElementById("pbFetchUrl")
 pbFetchUrl.addEventListener("click", actionFetchUrl)
-
-async function actionFetchUrl(btn) {
-    const url = inpUrl.value
-    console.log(url)
-    const jsonOutput = await fetchAnyUrl(url)
-    textArea.textContent = jsonOutput
-    console.log(jsonOutput)
-
-    function fetchAnyUrl(url) {
-        console.log("Inside fetch url = " + url)
-        return fetch(url).then(response  => response.json());
-    }
-
-    function fetchAnyUrlText(url) {
-        console.log("Inside fetch text url = " + url)
-        return fetch(url).then(response  => response.text());
-    }
-
-}
 
 
